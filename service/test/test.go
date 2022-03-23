1a015f00f5430967d17a5d0f92522324f5e418f1
袁彪良 <yuanbiaoliang@rong360.com>
2022-03-23 22:11:06 +0800

    t

--- service/test/test.go
+++ service/test/test.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"bytes"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -14,19 +16,27 @@ func Get(c *gin.Context) {
 }
 
 func Post(c *gin.Context) {
+	//Gin框架，body参数只能读取一次
+	b, _ := c.GetRawData()
+	jsonStr := string(b)
+	logrus.WithFields(logrus.Fields{"point": "1647949841", "params": jsonStr}).Info()
 
+	//把读过的字节流重新放到body
+	c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(b))
+
+	//参数绑定
 	type postJson struct {
 		Name string `json:"name"`
 		Age  int    `json:"age"`
 	}
 	var p postJson
-	if err := c.BindJSON(&p); err != nil {
-		b, _ := c.GetRawData()
-		logrus.WithFields(logrus.Fields{
-			"point":  "1647949841",
-			"err":    "BindJSON err " + err.Error(),
-			"params": string(b),
-		}).Fatal()
+	if err := c.ShouldBindJSON(&p); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 1003,
+			"type": "post",
+			"msg":  err.Error(),
+			"data": jsonStr,
+		})
 		return
 	}
 
